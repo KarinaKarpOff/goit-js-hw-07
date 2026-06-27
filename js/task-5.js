7da96295cc724785cc5111e40d0f5c261045163f
Karina KarpOff <supershinshila1027@gmail.com>
2026-06-27 19:19:59 +0300

    #commit1

---
+++ js/task-5.js
@@ -0,0 +1,15 @@
+const ChangeColorBtn = document.querySelector(".change-color");
+const colorText = document.querySelector(".color");
+
+ChangeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+  colorText.textContent = randomColor;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
